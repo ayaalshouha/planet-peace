82e008afc75252897db02933bd4f7a094f2d280f
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-23 15:57:27 +0300

    edit hero-section styling

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent, LandingPageComponent],
+  imports: [HeaderComponent, LandingPageComponent, FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
--- src/app/home-page/landing-page/landing-page.component.ts
+++ src/app/home-page/landing-page/landing-page.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
-import { HeroSectionComponent } from "./hero-section/hero-section.component";
-import { TakeCareComponent } from "./take-care/take-care.component";
-import { MostPopularComponent } from "./most-popular/most-popular.component";
-import { TrendigCategoriesComponent } from "./trendig-categories/trendig-categories.component";
-import { SummerPlanetComponent } from "./summer-planet/summer-planet.component";
-import { NewsLetterComponent } from "./news-letter/news-letter.component";
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { TakeCareComponent } from './take-care/take-care.component';
+import { MostPopularComponent } from './most-popular/most-popular.component';
+import { TrendigCategoriesComponent } from './trendig-categories/trendig-categories.component';
+import { SummerPlanetComponent } from './summer-planet/summer-planet.component';
+import { NewsLetterComponent } from './news-letter/news-letter.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HeroSectionComponent, TakeCareComponent, MostPopularComponent, TrendigCategoriesComponent, SummerPlanetComponent, NewsLetterComponent],
+  imports: [HeroSectionComponent, TakeCareComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrl: './landing-page.component.css',
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
