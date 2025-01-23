ea4c6f934ec1f9dbcc5e0d5db25e6134e9576fd4
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-23 15:13:36 +0300

    import homepage main components

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, LandingPageComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
