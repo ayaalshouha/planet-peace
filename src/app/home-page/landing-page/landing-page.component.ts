385cfdc207a8670d2c479d719260ad19f3da8b48
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-23 15:12:24 +0300

    import landing page components

--- src/app/home-page/landing-page/landing-page.component.ts
+++ src/app/home-page/landing-page/landing-page.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { TakeCareComponent } from "./take-care/take-care.component";
+import { MostPopularComponent } from "./most-popular/most-popular.component";
+import { TrendigCategoriesComponent } from "./trendig-categories/trendig-categories.component";
+import { SummerPlanetComponent } from "./summer-planet/summer-planet.component";
+import { NewsLetterComponent } from "./news-letter/news-letter.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [HeroSectionComponent, TakeCareComponent, MostPopularComponent, TrendigCategoriesComponent, SummerPlanetComponent, NewsLetterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
