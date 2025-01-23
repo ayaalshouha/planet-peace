5588b06e4595cec1f338e27977b56a997d1072ca
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-23 22:26:41 +0300

    add plant request in service using http client and trefle token

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -1,4 +1,5 @@
 export const enviroment = {
   production: false,
   apiBaseUrl: 'https://trefle.io/api/v1',
+  trefleToken: 'M3SHbQxnA8z1-L5qmBKZyobRDV-5BXpqVZnwhdAxsaY',
 };
--- src/app/services/plants.service.ts
+++ src/app/services/plants.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { enviroment } from '../enviroments/enviroment';
 
 @Injectable({ providedIn: 'root' })
 export class PlantsService {
   constructor(private http: HttpClient) {}
 
-  // all(): Observable<any> {
-  //   // return this.http.get();
-  // }
+  all(): Observable<any> {
+    return this.http.get(
+      `${enviroment.apiBaseUrl}/plants?token=${enviroment.trefleToken}`
+    );
+  }
 }
