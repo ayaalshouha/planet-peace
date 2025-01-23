429720d2ab78cb7fe27dbb73444034cfd89b7db5
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-23 14:59:49 +0300

    update landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LandingPageComponent } from "./home-page/landing-page/landing-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomePageComponent],
+  imports: [HomePageComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
