6eed8bff52179282012fe03ebb6982e44af836f5
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-25 08:28:55 +0300

    update most popular plants display

--- src/app/home-page/landing-page/most-popular/most-popular.component.ts
+++ src/app/home-page/landing-page/most-popular/most-popular.component.ts
@@ -22,7 +22,7 @@ export class MostPopularComponent implements OnInit, OnDestroy {
       .subscribe({
         next: (response) => {
           this.plants.set(response.data);
-          this.limited.set(this.plants().slice(8, 16));
+          this.limited.set(this.plants().slice(5, 13));
         },
         error: (err) => {
           console.error('Error fetching plants:', err);
