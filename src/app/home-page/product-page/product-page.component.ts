0ddcb6d223a62c29cd35f261ac746f77b095ec9b
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-25 16:00:04 +0300

    import plant section into product page

--- src/app/home-page/product-page/product-page.component.ts
+++ src/app/home-page/product-page/product-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PlantComponent } from "./plant/plant.component";
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [],
+  imports: [PlantComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css',
 })
