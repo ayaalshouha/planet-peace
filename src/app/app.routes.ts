c2a25ccdb9e6119d3578003edae0ce7db7f25ef6
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-25 08:40:03 +0300

    edit imports in app componant template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LandingPageComponent } from './home-page/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    children: [
+      {
+        path: '',
+        component: LandingPageComponent,
+      },
+      {
+        path: 'product-page',
+        component: LandingPageComponent,
+      },
+    ],
+  },
+];
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent, LandingPageComponent, FooterComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
