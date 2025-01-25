a48bb850d8857b02c2f70dafaf4688cd34aa3112
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-25 11:12:07 +0300

    add footer template and css

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,7 +7,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
