b580f6d6525641e2739c559d2408498c56ff8176
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-26 20:43:18 +0300

    update

--- src/app/home-page/product-page/plant/plant.component.ts
+++ src/app/home-page/product-page/plant/plant.component.ts
@@ -14,16 +14,6 @@ export class PlantComponent implements OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['plant'] && changes['plant'].currentValue) {
       this.plant = this.plant ? JSON.parse(this.plant) : null;
-      console.log('Updated plant:', this.plant);
     }
   }
-
-  // private _plant!: string; // For storing the serialized plant string
-  // deserializedPlant: any = null; // For the deserialized object
-  // @Input() set plant(serializedPlant: string) {
-  //   this._plant = serializedPlant;
-  //   this.selectedPlant = this.selectedPlant
-  //     ? JSON.parse(serializedPlant)
-  //     : null;
-  // }
 }
