853119414bc54379a368d9e07b4b6dc5854517c1
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-26 22:11:03 +0300

    add burger icon to header and add responsive media query

--- src/app/home-page/header/header.component.ts
+++ src/app/home-page/header/header.component.ts
@@ -5,8 +5,14 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  toggleMenu() {
+    const burgerIcon = document.querySelector('.burger-icon');
+    const menu = document.querySelector('.menu');
 
+    burgerIcon?.classList.toggle('open');
+    menu?.classList.toggle('open');
+  }
 }
